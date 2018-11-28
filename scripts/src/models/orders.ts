0aaa35f44cb92fb567da88b61c3745c5cb593ed4
Yossi Rizgan <yosriz@gmail.com>
2018-11-28 09:35:11 +0200

    bug fix - cap calucaltion is based on all earns
    fix 'countToday' method to count only marketplace earn orders and not all user earn orders

--- scripts/src/models/orders.ts
+++ scripts/src/models/orders.ts
@@ -87,12 +87,13 @@ export class Order extends CreationDateModel {
 		return query.getCount();
 	}
 
-	public static countToday(userId: string, type: OfferType): Promise<number> {
+	public static countToday(userId: string, type: OfferType, origin: OrderOrigin): Promise<number> {
 		const midnight = new Date((new Date()).setUTCHours(0, 0, 0, 0));
 		const query = Order.createQueryBuilder()
 			.where("user_id = :userId", { userId })
 			.andWhere("type = :type", { type })
 			.andWhere("current_status_date > :midnight", { midnight })
+			.andWhere("origin = :origin", { origin })
 			.andWhere(new Brackets(qb => {
 				qb.where("status = :completed", { completed: "completed" })
 					.orWhere(
